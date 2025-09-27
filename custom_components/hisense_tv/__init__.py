8a439b4024a5cd776fc94140737b664d358fec15
Sebastian Franz <sebastian.franz@online.de>
2025-09-27 02:26:11 +0200

    Fixes for newer HA version

--- custom_components/hisense_tv/__init__.py
+++ custom_components/hisense_tv/__init__.py
@@ -20,7 +20,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     for platform in PLATFORMS:
         hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, platform)
+            hass.config_entries.async_forward_entry_setups(entry, platform)
         )
 
     return True
